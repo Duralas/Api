0f666df376f47116951f3e88b014fba9ac2b7ec0
Edhrendal <devrendal@protonmail.com>
2021-10-12 00:39:04 +0200

    🎊 Symfony init

---
+++ src/Controller/HealthCheck/GetController.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\HealthCheck;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class GetController
+{
+    #[Route('/health-check', name: 'app.other.health-check.get', methods: ['GET'])]
+    public function __invoke(): JsonResponse
+    {
+        return new JsonResponse(['status' => 'OK', 'message' => 'Ok.']);
+    }
+}
